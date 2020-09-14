ee81fbef4ff6fc29c4163ef8887ba5df7e87ea93
Trafalgar99 <1286015267@qq.com>
2020-09-14 22:30:50 +0800

    9.13

---
+++ 基础算法一/3次方根.cpp
@@ -0,0 +1,23 @@
+// https://www.acwing.com/problem/content/description/792/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    double n;
+    scanf("%lf", &n);
+    double l = -10000, r = 10000;
+    while (r - l >= 1e-8)
+    {
+        double mid = (l + r) / 2;
+        if (mid * mid * mid >= n)
+            r = mid;
+        else
+            l = mid;
+    }
+
+    printf("%f", l);
+    return 0;
+}
---
+++ 基础算法一/3次方根.py
@@ -0,0 +1,13 @@
+// https: // www.acwing.com/problem/content/description/792/
+
+n = float(input())
+l, r = -10000, 10000.0
+
+while r-l > 1e-12:
+    mid = (l+r)/2
+    if mid**3 >= n:
+        r = mid
+    else:
+        l = mid
+
+print("%.6f" % (l))
