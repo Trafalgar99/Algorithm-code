77fd0446c1d22a44d2e7faf9816d1e00100807ad
Trafalgar99 <1286015267@qq.com>
2020-09-16 22:35:02 +0800

    9.16

--- practice.cpp
+++ practice.cpp
@@ -1,52 +1,20 @@
-#include <iostream>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 
-void quick_sort(int q[], int l, int r)
+vector<int> div(vector<int> &A, int b, int &r)
 {
-    if (l >= r)
-        return;
-    int i = l - 1, j = r + 1, x = q[l + r >> 1];
-    while (i < j)
+    vector<int> C;
+    r = 0;
+    for (int i = A.size(); i >= 0; i--)
     {
-        do
-            i++;
-        while (q[i] < x);
-        do
-            j--;
-        while (q[j] > x);
-        if (i < j)
-            swap(q[i], q[j]);
+        r = r * 10 + A[i];
+        C.push_back(r / 10);
+        r %= 10;
     }
-    quick_sort(q, l, j);
-    quick_sort(q, j + 1, r);
-}
-int tmp[];
-void merge_sort(int q[], int l, int r)
-{
-    if (l >= r)
-        return;
-    int mid = l + r >> 1;
-    merge_sort(q, l, mid), merge_sort(q, mid + 1, r);
-
-    int k = 0, i = l, j = mid + 1;
-    while (i <= mid, j <= r)
-    {
-        if (q[i] <= q[j])
-            tmp[k++] = q[i++];
-        else
-            tmp[k++] = q[j++];
-    }
-    while (i <= mid)
-        tmp[k++] = q[i++];
-    while (j <= mid)
-        tmp[k++] = q[j++];
-
-    for (i = l, j = 0; i <= r; i++, j++)
-        q[i] = tmp[j];
-}
-
-int main()
-{
-    return 0;
+    reverse(C.begin(), C.end());
+    while (C.size() > 1 && C.back() == 0)
+        C.pop_back();
+    return C;
 }
