0dd56129221ab63d01f2406985ac7c563ba1b934
Trafalgar99 <1286015267@qq.com>
2020-10-07 21:18:25 +0800

    2020年10月 7日 21:18:24

--- 习题1/逆序对的数量.cpp
+++ 习题1/逆序对的数量.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
 
-typedef long long LL;
-
 using namespace std;
+
+typedef long long LL;
 
 const int N = 100010;
 
@@ -16,6 +16,7 @@ LL merge_sort(int l, int r)
     int mid = l + r >> 1;
     LL res = merge_sort(l, mid) + merge_sort(mid + 1, r);
 
+    //merge
     int k = 0, i = l, j = mid + 1;
     while (i <= mid && j <= r)
         if (q[i] <= q[j])
@@ -23,26 +24,28 @@ LL merge_sort(int l, int r)
         else
         {
             tmp[k++] = q[j++];
-            res += mid - l + 1;
+            res += mid - i + 1;
         }
+
     while (i <= mid)
         tmp[k++] = q[i++];
     while (j <= r)
         tmp[k++] = q[j++];
 
-    for (int i = l, j = 0; i <= r; j++, i++)
+    for (int i = l, j = 0; i <= r; i++, j++)
         q[i] = tmp[j];
-
     return res;
 }
 
 int main()
 {
+
     cin >> n;
     for (int i = 0; i < n; i++)
         cin >> q[i];
 
     cout << merge_sort(0, n - 1) << endl;
-
+    getchar();
+    getchar();
     return 0;
 }
